4284eac30d480393e2dd46077e717922f6c8bf79
Sarah <sarah.jehin@belgacom.net>
2017-02-26 15:28:48 +0100

    init laravel project

---
+++ database/migrations/2017_02_26_131003_create_activities_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateActivitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('description');
+            $table->string('poster');
+            $table->string('extra_url')->nullable();
+            $table->dateTime('startdate');
+            $table->dateTime('deadline')->nullable();
+            $table->string('location');
+            $table->integer('min_participants')->nullable();
+            $table->integer('max_participants')->nullable();
+            $table->integer('helpers')->nullable();
+            $table->decimal('price', 6, 2)->nullable();
+            $table->tinyInteger('youth_adult');
+            $table->tinyInteger('is_visible');
+            $table->integer('category_id')->unsigned()->index();
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->integer('made_by_id')->unsigned()->index();
+            $table->foreign('made_by_id')->references('id')->on('users');
+            $table->integer('owner_id')->unsigned()->index();
+            $table->foreign('owner_id')->references('id')->on('users');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activities');
+    }
+}
---
+++ database/migrations/2017_02_26_140009_create_activity_user_pivot_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateActivityUserPivotTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activity_user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('activity_id')->unsigned()->index();
+            $table->foreign('activity_id')->references('id')->on('activities');
+            $table->tinyInteger('helper_participant');
+            $table->tinyInteger('paid');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activity_user');
+    }
+}
